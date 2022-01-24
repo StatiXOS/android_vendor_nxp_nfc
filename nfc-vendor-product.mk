4b881aed11d874142fb50920e5ca1541f85826d6
Alexander Koskovich <akoskovich@pm.me>
2022-01-24 09:38:32 -0700

    nfc: Copy some default configurations based off NFC hardware.
    Change-Id: I54e95bd6751c4d7daff7af465b9e207cffdf9c22

--- nfc-vendor-product.mk
+++ nfc-vendor-product.mk
@@ -4,6 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+NXP_NFC_HARDWARE := PN81T
+NXP_VENDOR_PATH := vendor/nxpi
+
+PRODUCT_COPY_FILES += \
+    $(NXP_VENDOR_PATH)/nfc/halimpl/libnfc-nxp-$(NXP_NFC_HARDWARE)_example.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(NXP_VENDOR_PATH)/nfc/halimpl/libnfc-nxp_RF-$(NXP_NFC_HARDWARE)_example.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf \
+    $(NXP_VENDOR_PATH)/secure_element/libese-spi/p73/libese-nxp-P73.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libese-nxp.conf
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
