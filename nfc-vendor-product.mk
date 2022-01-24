905ab8ef9f0aa79ae16b6759c028359da5f9071d
Alexander Koskovich <akoskovich@pm.me>
2022-01-24 09:25:18 -0700

    nfc: Create a split makefile for PN8X NXP NFC.
     * This covers basic permissions, NFC v1.2 HAL and
       Secure v1.2 HAL.
    
     * Let eSE permissions be excluded with 'PRODUCT_USES_ESE'
       in the event of the device not supporting eSE. In
       the event of the ZenFone 8 only the Japan SKU supports
       eSE.
    
    Change-Id: I88dd8b5fa58527ee4a7766fdd67ca3990965b1fb

---
+++ nfc-vendor-product.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+
+ifneq ($(PRODUCT_USES_ESE),false)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml
+endif
+
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service.pn8x \
+    android.hardware.secure_element@1.2-service.p73
